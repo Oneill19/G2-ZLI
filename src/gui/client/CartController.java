0ac6a3c9b7fceb78cfeeca36c9a4e2b0b94e2005
DorinBe <dorinbeery@gmail.com>
2022-05-20 21:37:13 +0100

    fix delete button to recognize selected row and remove it

--- src/gui/client/CartController.java
+++ src/gui/client/CartController.java
@@ -1,7 +1,5 @@
 package gui.client;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -11,12 +9,14 @@ import java.util.ArrayList;
 
 import entity.AbstractProduct;
 import entity.Item;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -27,8 +27,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -58,7 +56,8 @@ public class CartController {
 	private TableColumn<AbstractProduct, String> colName;
 	@FXML
 	private TableColumn<AbstractProduct, Double> colPrice;
-    @FXML private TextField textFieldPrice;
+	@FXML
+	private TextField textFieldPrice;
 	@FXML
 	private Button exit;
 	@FXML
@@ -69,16 +68,15 @@ public class CartController {
 	private Button onBack;
 	@FXML
 	private Button userOptBtn;
-	private boolean flag=false;
+	private boolean flag = false;
 
-	
 	@FXML
 	void onBack(ActionEvent event) throws IOException {
-    	((Node) event.getSource()).getScene().getWindow().hide();
-    	Stage primaryStage = new Stage();
-    	new FXMLLoader();
-    	Pane root = FXMLLoader.<Pane>load(getClass().getResource("Catalog.fxml"));
-    	Scene scene = new Scene(root);
+		((Node) event.getSource()).getScene().getWindow().hide();
+		Stage primaryStage = new Stage();
+		new FXMLLoader();
+		Pane root = FXMLLoader.<Pane>load(getClass().getResource("Catalog.fxml"));
+		Scene scene = new Scene(root);
 		primaryStage.setTitle("Zer-Li Catalog");
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -94,6 +92,12 @@ public class CartController {
 
 	}
 
+	/**
+	 * [data] is received from Catalog if Catalog is not ready then method
+	 * "bypassCatalog" is used.
+	 * 
+	 * @param data includes list of products selected by user from catalog.
+	 */
 	void initData(ArrayList<AbstractProduct> data) {
 		productsInCatalog = data;
 		observableList = FXCollections.observableArrayList(productsInCatalog);
@@ -101,71 +105,120 @@ public class CartController {
 
 	public void initialize() {
 
-		if (flag==false)
+		if (flag == false)
 			bypassCatalog();
 
-		// add cell of button delete
-		Callback<TableColumn<AbstractProduct, String>, TableCell<AbstractProduct, String>> cellFoctory = (
-				TableColumn<AbstractProduct, String> param) -> {
-			// make cell containing buttons
-			final TableCell<AbstractProduct, String> cell = new TableCell<AbstractProduct, String>() {
-				@Override
-				public void updateItem(String item, boolean empty) {
-					File deleteIconFile=null;
-					FileInputStream fisDeleteIcon=null;
-					super.updateItem(item, empty);
-					
-					// that cell created only on non-empty rows
-					if (empty) {
-						setGraphic(null);
-						setText(null);
-					} else {
-						try {
-							deleteIconFile = new File("C:\\Users\\Dorin\\eclipse-workspace\\Git\\G2-ZLI\\src\\gui\\client\\delete.png");
-							fisDeleteIcon = new FileInputStream(deleteIconFile);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						Button deleteButton = new Button("", new ImageView(new Image(fisDeleteIcon)));
-						deleteButton.setOnMouseClicked((MouseEvent event) -> {
-							try {
-								AbstractProduct product = cartTable.getSelectionModel().getSelectedItem(); //get focused row
-								cartTable.getItems().remove(product); //remove focused product from list of product
-								Double sumOfProducts= new Double(0);	
-								for (AbstractProduct ai : cartTable.getItems())
-									sumOfProducts += ai.getPrice(); 	
-								textFieldPrice.setText(sumOfProducts.toString());
-							} catch (Exception ex) {
-								ex.printStackTrace();
-							}
-						});
+		//Sets colDelete behavior
+		colDelete.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<AbstractProduct, String>, ObservableValue<String>>() {
 
-						HBox managebtn = new HBox(deleteButton);
-						managebtn.setStyle("-fx-alignment:center");
-						HBox.setMargin(deleteButton, new Insets(2, 2, 0, 3));
-						setGraphic(managebtn);
-						setText(null);
+					@Override
+					public ObservableValue<String> call(TableColumn.CellDataFeatures<AbstractProduct, String> p) {
+						return new SimpleStringProperty(p.getValue() != null, null);
 					}
-				}
-			};
-			return cell;
-		};
+				});
 
-		colDelete.setCellFactory(cellFoctory);
+
+		//create class DeleteButton
+		class DeleteButton extends TableCell<AbstractProduct, String> {
+			private Button deleteButton = null;
+
+			DeleteButton() {
+				deleteButton = new Button("", new ImageView(new Image(getClass().getResourceAsStream("delete.png"))));
+
+				// Action when the button is pressed
+				deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+
+					@Override
+					public void handle(ActionEvent t) {
+						// get Selected Item
+						AbstractProduct currentAbstractProduct = (AbstractProduct) DeleteButton.this.getTableView()
+								.getItems().get(DeleteButton.this.getIndex());
+						// remove selected item from the table list
+						observableList.remove(currentAbstractProduct);
+						Double totalPrice = new Double(0);
+						for (AbstractProduct ap : observableList)
+							totalPrice += ap.getPrice();
+						textFieldPrice.setText(totalPrice.toString());
+					}
+				});
+			}
+
+			// Display button if the row is not empty
+			@Override
+			public void updateItem(String t, boolean empty) {
+				super.updateItem(t, empty);
+				if (empty) {
+					setGraphic(null);
+					setText(null);
+				} else {
+					setGraphic(deleteButton);
+				}
+			}
+		}
+
+		// new
+		// Adding the Button to the cell
+		colDelete.setCellFactory(
+				new Callback<TableColumn<AbstractProduct, String>, TableCell<AbstractProduct, String>>() {
+
+					@Override
+					public TableCell<AbstractProduct, String> call(TableColumn<AbstractProduct, String> p) {
+						return new DeleteButton();
+					}
+
+				});
+
+		/*
+		 * // add cell of button delete Callback<TableColumn<AbstractProduct, String>,
+		 * TableCell<AbstractProduct, String>> cellFoctory = (
+		 * TableColumn<AbstractProduct, String> param) -> { // make cell containing
+		 * buttons final TableCell<AbstractProduct, String> cell = new
+		 * TableCell<AbstractProduct, String>() {
+		 * 
+		 * @Override public void updateItem(String item, boolean empty) { File
+		 * deleteIconFile = null; FileInputStream fisDeleteIcon = null;
+		 * super.updateItem(item, empty);
+		 * 
+		 * // that cell created only on non-empty rows if (empty) { setGraphic(null);
+		 * setText(null); } else { try { deleteIconFile = new File(
+		 * "C:\\Users\\Dorin\\eclipse-workspace\\Git\\G2-ZLI\\src\\gui\\client\\delete.png"
+		 * ); fisDeleteIcon = new FileInputStream(deleteIconFile); } catch (Exception e)
+		 * { e.printStackTrace(); } Button deleteButton = new Button("", new
+		 * ImageView(new Image(fisDeleteIcon)));
+		 * deleteButton.setId((incrementalNumber++).toString());
+		 * deleteButton.setOnMouseClicked((MouseEvent event) -> { try { //
+		 * System.out.println(cartTable.getSelectionModel().getSelectedIndex()); //
+		 * //prints -1 System.out.println(event.getPickResult()); AbstractProduct
+		 * product = cartTable.getSelectionModel().getSelectedItem(); // get
+		 * 
+		 * // row cartTable.getItems().remove(product); // remove focused product from
+		 * list of product Double sumOfProducts = new Double(0); for (AbstractProduct ai
+		 * : cartTable.getItems()) sumOfProducts += ai.getPrice();
+		 * textFieldPrice.setText(sumOfProducts.toString()); } catch (Exception ex) {
+		 * ex.printStackTrace(); } });
+		 * 
+		 * HBox managebtn = new HBox(deleteButton);
+		 * managebtn.setStyle("-fx-alignment:center"); HBox.setMargin(deleteButton, new
+		 * Insets(2, 2, 0, 3)); setGraphic(managebtn); setText(null);
+		 * 
+		 * } } }; return cell; };
+		 */
+
+		// colDelete.setCellFactory(cellFoctory);
 		colImage.setCellValueFactory(new PropertyValueFactory<>("image"));
 		colName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
 		cartTable.setId("my-table");
 
 		cartTable.getItems().clear();
-		cartTable.setItems(FXCollections.observableArrayList(productsInCatalog));
-		
-		
-		Double sumOfProducts= new Double(0);	
+		cartTable.setItems(observableList);
+
+		Double sumOfProducts = new Double(0);
 		for (AbstractProduct ai : cartTable.getItems())
-			sumOfProducts += ai.getPrice(); 	
+			sumOfProducts += ai.getPrice();
 		textFieldPrice.setText(sumOfProducts.toString());
-		
+
 	}
 
 	/*
@@ -199,7 +252,7 @@ public class CartController {
 			}
 			productsInCatalog = list;
 			observableList = FXCollections.observableArrayList(productsInCatalog);
-			flag=true;
+			flag = true;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
