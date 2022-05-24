57b0062d058d4cf2e220927fcd42e5df7e9b65f0
DorinBe <90141260+DorinBe@users.noreply.github.com>
2022-05-24 18:32:50 +0100

    prototype of personalDetails screen and controller

---
+++ src/gui/client/PersonalDetailsController.java
@@ -0,0 +1,148 @@
+package gui.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class PersonalDetailsController {
+
+    @FXML
+    private ToggleGroup DeliveryMethod;
+
+    @FXML
+    private RadioButton DeliveryRadio;
+
+    @FXML
+    private ComboBox<?> comboStore;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private TextField fieldAptNumber;
+
+    @FXML
+    private TextField fieldBlessing;
+
+    @FXML
+    private TextField fieldCity;
+
+    @FXML
+    private TextField fieldEmail;
+
+    @FXML
+    private TextField fieldFirst;
+
+    @FXML
+    private TextField fieldLast;
+
+    @FXML
+    private TextField fieldPostal;
+
+    @FXML
+    private TextField fieldSt;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button onBack;
+
+    @FXML
+    private RadioButton pickUpRadio;
+
+    @FXML
+    private TextField timePicker;
+
+    @FXML
+    private Button userOptBtn;
+
+    @FXML
+    void onAptNumber(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBlessing(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDatePicker(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDelivery(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEmail(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onFirst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLast(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPickUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPostal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStore(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTimePicker(ActionEvent event) {
+
+    }
+
+}
