dd120879fa4755bdf3656015f531f41401588ce7
DorinBe <90141260+DorinBe@users.noreply.github.com>
2022-06-02 22:39:46 +0100

    add prototype of markeyingemployeecontroller.fxml

--- src/gui/client/MarketingEmployeeOptionsController.java
+++ src/gui/client/MarketingEmployeeOptionsController.java
@@ -1,5 +1,31 @@
 package gui.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MarketingEmployeeOptionsController {
+
+    @FXML private Button onBack, logoutBtn, userOptBtn, exit, addSaleButton, watchSaleButton, nextBtn;
+
+    @FXML
+    void onBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNext(ActionEvent event) {
+
+    }
 
 }
