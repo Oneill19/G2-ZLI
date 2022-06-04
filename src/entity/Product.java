e0b0f5ae69016ced31909a29f3af0ae17bac18a9
DorinBe <90141260+DorinBe@users.noreply.github.com>
2022-06-04 16:04:22 +0100

    the missing files

---
+++ src/entity/Product.java
@@ -0,0 +1,70 @@
+package entity;
+
+import java.util.ArrayList;
+
+/**
+ * @AUTHOR ONEILL PANKER
+ *
+ */
+@SuppressWarnings("serial")
+public class Product extends AbstractProduct {
+
+	private String other;
+	private ArrayList<Item> madeFrom;
+
+	
+	public Product() {
+		super();
+	}
+	/**
+	 * @param serialNumber
+	 * @param name
+	 * @param price
+	 * @param type
+	 * @param image
+	 * @param other
+	 * @param madeFrom
+	 */
+
+	public Product(String serialNumber, String name, double price, String type, String image, String other,
+			ArrayList<Item> madeFrom, int sale) {
+		super(serialNumber, name, price, image, false, type, sale);
+		this.other = other;
+		this.madeFrom = madeFrom;
+	}
+	
+	public Product(Product ap) {
+		super(ap);
+		this.other=  ap.getOther();
+		this.madeFrom = ap.getMadeFrom();
+	}
+
+	/**
+	 * @return the other
+	 */
+	public String getOther() {
+		return other;
+	}
+
+	/**
+	 * @param other the other to set
+	 */
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	/**
+	 * @return the madeFrom
+	 */
+	public ArrayList<Item> getMadeFrom() {
+		return madeFrom;
+	}
+
+	/**
+	 * @param madeFrom the madeFrom to set
+	 */
+	public void setMadeFrom(ArrayList<Item> madeFrom) {
+		this.madeFrom = madeFrom;
+	}
+
+}
