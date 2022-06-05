80d06ab617815267f43774965c79417fdb588975
Oneill Panker <oneill.p.19@gmail.com>
2022-06-05 22:07:41 +0300

    delete not used files

--- src/common/MyImage.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class MyImage implements Serializable{
-
-}
--- src/common/MyListener.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-import entity.AbstractProduct;
-
-public interface MyListener {
-	public void onClickListener(AbstractProduct product);
-}
